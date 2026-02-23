36995c2a477977bb750a2fdcbf4a9d7667e7ae47
Ayushman2005 <karayushman736@gmail.com>
2026-02-23 16:59:57 +0530

    23/02/2026

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -6,7 +6,7 @@
 
 import axios from 'axios'
 
-const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000'
+const BASE_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:8000";
 
 const client = axios.create({
   baseURL: BASE_URL,
